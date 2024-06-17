6940cd85a777289978de4b3646372b75a496a3c3
rahulkush1479 <165028502+rahulkush1479@users.noreply.github.com>
2024-06-18 00:09:27 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+
+function myFunction(x) {
+    $('#toggleiconbar,#toggleiconx').toggleClass("fa-xmark");
+}
+
+
+function addclass() {
+    $('#togglepart').toggleClass('toggleshow')
+}
+
+
+$('.owl-imgslideno').owlCarousel({
+    loop: false,
+    margin: 20,
+    nav: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        900: {
+            items: 4
+        },
+        1400: {
+            items: 4
+        }
+    }
+})
+
+$('.owl-img').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        900: {
+            items: 3
+        },
+        1400: {
+            items: 4
+        }
+    }
+})
